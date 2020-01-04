a02a833011ec749036ba3f9fa8728cb6c422efd4
Ofir Levi <ofirl24@gmail.com>
2020-01-04 14:24:28 +0200

    log test

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    console.log('webhook called!');
+    res.send('respond with a resource');
+});
+
+router.post('/', function (req, res, next) {
+    console.log('webhook called!');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
